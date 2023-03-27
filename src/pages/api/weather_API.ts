8033c64389b009230c856bdca590fb0e4eb577d6
Andrew <xxxandas@gmail.com>
2023-03-27 23:58:24 +0700

    hotfix 1

--- src/pages/api/weather_API.ts
+++ src/pages/api/weather_API.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const WEATHER_API = "55323c84582082191f5c61a44a6abfbc";
-const DEFAULT_WEATHER_API_URL = "http://api.openweathermap.org/data/2.5";
+const DEFAULT_WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 const WEATHER_API_URL = "?appid=" + WEATHER_API;
 const IPINFO_API = "5e7f57a3bc1fc3";
 const DEFAULT_IPINFO_API_URL = "https://ipinfo.io";
